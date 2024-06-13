b2365c52d14293c7e8e87154faf847a447642219
Ken <kenlane33@gmail.com>
2024-06-13 11:09:42 -0400

    [Build JS Now] and [Enter My Api Key] buttons

--- Editor/MultisynqEditorWindow/MultisynqWelcome.cs
+++ Editor/MultisynqEditorWindow/MultisynqWelcome.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -37,6 +38,7 @@ public class Colz {
   public Color blue;
   public Color lime;
   public Color white;
+  public Color grey;
   public Color c_node;
 
   public Colz() {
@@ -46,6 +48,7 @@ public class Colz {
     blue   = GetColor("#006AFF");
     lime   = GetColor("#00FF00");
     white  = GetColor("#FFFFFF");
+    grey   = GetColor("#888888");
     c_node = GetColor("#417E37");
   }
 
@@ -90,12 +93,22 @@ public class MultisynqWelcomeEW : EditorWindow {
     public Status blank;
     public Label label;
     public VisualElement img;
-    public StatusSet(Label label, VisualElement img, string _info, string _warning, string _error, string _success) {
+    public bool IsOk() {
+      return (status == "ready") || (status == "success");
+    }
+    public void SuccessToReady() {
+      if (status == "success") {
+        status = "ready";
+        ready.Set();
+      }
+    }
+
+    public StatusSet(Label label, VisualElement img, string _info, string _warning, string _error, string _success, string _blank) {
       ready   = new Status("ready",   label, img, _info,    colz.green,  this);
       warning = new Status("warning", label, img, _warning, colz.yellow, this);
       error   = new Status("error",   label, img, _error,   colz.red,    this);
       success = new Status("success", label, img, _success, colz.lime,   this);
-      blank   = new Status("blank",   label, img, "--",     colz.white,  this);
+      blank   = new Status("blank",   label, img, _blank,   colz.grey,   this);
     }
   }
   //=============================================================================
@@ -105,6 +118,14 @@ public class MultisynqWelcomeEW : EditorWindow {
     static public StatusSet key;
     static public StatusSet jsBuild;
     static public StatusSet settings;
+
+    static public void SuccessesToReady() {
+      ready.SuccessToReady();
+      node.SuccessToReady();
+      key.SuccessToReady();
+      jsBuild.SuccessToReady();
+      settings.SuccessToReady();
+    }
   }
 
   private static Colz colz;
@@ -114,6 +135,8 @@ public class MultisynqWelcomeEW : EditorWindow {
   // public StatusSet statuses_key;
   // public StatusSet statuses_jsBuild;
   //=============================================================================
+  double lastTime = 0;
+  double countdown_ToConvertSuccesses = -1;
 
   private Button Awesome_Btn;        
   private Button TakeMeToSetting_Btn;
@@ -124,7 +147,8 @@ public class MultisynqWelcomeEW : EditorWindow {
   private Button CheckIfReady_Btn;
   private Button SettingsSelect_Btn;
   private Button SettingsCreate_Btn;
-
+  private Button EnterApiKey_Btn;
+  private Button Build_JsNow_Btn;
 
   private Label Ready_Message_Lbl;
   private Label Node_Message_Lbl;
@@ -137,7 +161,22 @@ public class MultisynqWelcomeEW : EditorWindow {
   private VisualElement Key_Status_Img;
   private VisualElement JSBuild_Status_Img;
   private VisualElement Settings_Status_Img;
+  private List<Button> allButtons = new();
 
+    void Update() {
+    if (lastTime == 0) lastTime = EditorApplication.timeSinceStartup;
+    var deltaTime = EditorApplication.timeSinceStartup - lastTime;
+    lastTime = EditorApplication.timeSinceStartup;
+
+    if (countdown_ToConvertSuccesses>0f) {
+      countdown_ToConvertSuccesses -= deltaTime;
+      Ready_Message_Lbl.text = Statuses.ready.success.message + "   <b>" + countdown_ToConvertSuccesses.ToString("0.0") + "</b>";
+      if (countdown_ToConvertSuccesses <= 0f) {
+        countdown_ToConvertSuccesses = -1;
+        Statuses.SuccessesToReady(); // <=======
+      }
+    }
+  }
   //=============================================================================
 
   // private const string ewFolder = "Assets/Scripts/Editor/MultisynqEditorWindow/";
@@ -200,11 +239,13 @@ public class MultisynqWelcomeEW : EditorWindow {
     SetupButton("CheckIfReady_Btn",    ref CheckIfReady_Btn,    Clk_CheckIfReady   );
     SetupButton("SettingsSelect_Btn",  ref SettingsSelect_Btn,  Clk_SettingsSelect );
     SetupButton("SettingsCreate_Btn",  ref SettingsCreate_Btn,  Clk_SettingsCreate );
+    SetupButton("EnterApiKey_Btn",     ref EnterApiKey_Btn,     Clk_EnterApiKey    );
+    SetupButton("Build_JsNow_Btn",     ref Build_JsNow_Btn,     Clk_Build_JsNow    );
 
     SetupLabel("Ready_Message_Lbl",    ref Ready_Message_Lbl   ); 
-    SetupLabel("Node_Message_Lbl",     ref Node_Message_Lbl   );
-    SetupLabel("Key_Message_Lbl",      ref Key_Message_Lbl    );
-    SetupLabel("JSBuild_Message_Lbl",  ref JSBuild_Message_Lbl);
+    SetupLabel("Node_Message_Lbl",     ref Node_Message_Lbl    );
+    SetupLabel("Key_Message_Lbl",      ref Key_Message_Lbl     );
+    SetupLabel("JSBuild_Message_Lbl",  ref JSBuild_Message_Lbl );
     SetupLabel("Settings_Message_Lbl", ref Settings_Message_Lbl);
 
     // SetupImage("Ready_Status_Img",     ref Ready_Status_Img  );
@@ -212,14 +253,25 @@ public class MultisynqWelcomeEW : EditorWindow {
     // SetupImage("Key_Status_Img",       ref Key_Status_Img    );
     // SetupImage("JSBuild_Status_Img",   ref JSBuild_Status_Img);
 
-    SetupVisElem("Ready_Status_Img",     ref Ready_Status_Img  );
-    SetupVisElem("Node_Status_Img",      ref Node_Status_Img   );
-    SetupVisElem("Key_Status_Img",       ref Key_Status_Img    );
-    SetupVisElem("JSBuild_Status_Img",   ref JSBuild_Status_Img);
+    SetupVisElem("Ready_Status_Img",     ref Ready_Status_Img   );
+    SetupVisElem("Node_Status_Img",      ref Node_Status_Img    );
+    SetupVisElem("Key_Status_Img",       ref Key_Status_Img     );
+    SetupVisElem("JSBuild_Status_Img",   ref JSBuild_Status_Img );
     SetupVisElem("Settings_Status_Img",  ref Settings_Status_Img);
 
-    // hide Awesome_Btn
-    Awesome_Btn.style.visibility = Visibility.Hidden;
+    // foreach (Button button in allButtons) {
+    //   string[] whitelisted = {
+    //     "CheckIfReady_Btn",
+    //     "_Docs_",
+    //   };
+    //   if (whitelisted.Any(button.name.Contains)) {
+    //     button.style.visibility = Visibility.Visible;
+    //   } else {
+    //     button.style.visibility = Visibility.Hidden;
+    //   }
+    // }
+
+    // Awesome_Btn.style.visibility = Visibility.Hidden;
   }
 
   private void SetupStatuses() {
@@ -233,35 +285,40 @@ public class MultisynqWelcomeEW : EditorWindow {
       $"You are <b><size=+1><color=#77ff77>Ready to </color>{t_synq}</b></size><color=#888>      All green lights below.",
       $"Warn 00000",
       $"Look below in \"Help Getting Set Up\" for what's not ready...",
-      $"W00t!!! You are ready to {t_synq}!" // displays for 5 seconds, then switches to the .ready message
+      $"W00t!!! You are ready to {t_synq}!", // displays for 5 seconds, then switches to the .ready message
+      "Press  Check If Ready"
     );
     Statuses.node = new StatusSet( Node_Message_Lbl, Node_Status_Img,
       // (info, warning, error, success)
       $"{t_node} is ready to go!",
       $"{t_node} is not running",
       $"{t_node} needs your help getting set up.",
-      $"{t_node} path configured!!! Well done!"
+      $"{t_node} path configured!!! Well done!",
+      "< Node status >"
     );
     Statuses.key = new StatusSet( Key_Message_Lbl, Key_Status_Img,
       // ... info, warning, error, success)
       $"The {t_key} is ready to go!",
       $"The {t_key} is not set",
       $"Let's get you a free {t_key}. It's easy.",
-      $"The {t_key} is configured!!! Well done!"
+      $"The {t_key} is configured!!! Well done!",
+      "< API Key status >"
     );
     Statuses.jsBuild = new StatusSet( JSBuild_Message_Lbl, JSBuild_Status_Img,
       // ... info, warning, error, success)
       $"{t_jsb} is ready to go!",
       $"{t_jsb} is not ready",
       $"{t_jsb} needs your help getting set up.",
-      $"{t_jsb} path configured!!! Well done!"
+      $"{t_jsb} path configured!!! Well done!",
+      "< JS Build status >"
     );
     Statuses.settings = new StatusSet( Settings_Message_Lbl, Settings_Status_Img,
       // ... info, warning, error, success)
       $"Settings are ready to go!",
       $"Settings are set to defaults! Look for other red items below to fix this.",
       $"Settings asset is missing! Click <b>Create Settings</b> to make some.",
-      $"Settings are configured!!! Well done!"
+      $"Settings are configured!!! Well done!",
+      "< Settings status >"
     );
     SettingsSelect_Btn.SetEnabled(false);
   }
@@ -269,105 +326,13 @@ public class MultisynqWelcomeEW : EditorWindow {
   //=============================================================================
   //=============================================================================
 
-  private void Clk_CheckIfReady() {
-    bool allRdy = true;
-    allRdy &= Check_Settings();
-    allRdy &= Check_Node();
-    //-----
-    if (allRdy) AllAreReady();
-    else        AllAreReady(false);
-  }
-
-  private void CheckAllStatusForReady() {
-    bool allRdy = true;
-    // NEVER: allRdy &= Statuses.ready.status    == "success";
-    allRdy &= Statuses.settings.status == "success";
-    allRdy &= Statuses.node.status     == "success";
-    // allRdy &= Statuses.key.status      == "success";
-    // allRdy &= Statuses.jsBuild.status  == "success";
-    if (allRdy) AllAreReady();
-    else        AllAreReady(false);
-  }
-  private void AllAreReady(bool really = true) {
-    if (really) {
-      Statuses.ready.success.Set();
-      Awesome_Btn.style.visibility = Visibility.Visible;
-    } else {
-      Statuses.ready.error.Set();
-      Awesome_Btn.style.visibility = Visibility.Hidden;
-    }
-  }
-
-  private bool Check_Node() {
-    var cqStgs = FindProjectCqSettings();
-    string nodePath = cqStgs.pathToNode;
-    string nodeVer = TryNodePath(nodePath);
-    if (nodeVer == null) {
-      Statuses.node.error.Set();
-      return false;
-    } else {
-      Statuses.node.success.Set();
-      return true;
-    }
-  }
-  private bool Check_Settings() {
-    var cqStgs = FindProjectCqSettings();
-    if (cqStgs == null) {
-      Statuses.settings.error.Set();
-      SettingsSelect_Btn.SetEnabled(false);
-      return false;
-    } else {
-      SettingsSelect_Btn.SetEnabled(true);
-      Statuses.settings.success.Set();
-      return true;
-    }
-  }
-
-
-  private void Clk_SettingsSelect() {
-    Clk_TakeMeToSetting();
-  }
-
-  private void Clk_SettingsCreate() {
-    // CroquetSettings in scene
-    var cqStgs = EnsureSettingsFile();
-    if (cqStgs == null) {
-      Debug.LogError("Could not find or create CroquetSettings file");
-      Statuses.ready.error.Set();
-    } else {
-      Statuses.ready.success.Set();
-    }
-    Check_Settings();
-    Clk_TakeMeToSetting();
-  }
-
-  private void Clk_SignUpApi() {
-    Clk_TakeMeToSetting();
-    Application.OpenURL("https://croquet.io/account/");
-  }
-  private void Clk_Top_Ready_Docs() {
-    Application.OpenURL("https://multisynq.io/docs/unity/");
-  }
-
   private void Clk_BeAwesome() {
     Debug.Log("Be Awesome!!!!");
     // Application.OpenURL("https://www.youtube.com/watch?v=dQw4w9WgXcQ"); // Copilot thinks you should go to this url. You know you want to. =]
     Application.OpenURL("https://giphy.com/search/everything-is-awesome");
   }
-  private async void Clk_ToggleJSBuild() {
-    if (ToggleJSBuild_Btn.text == "Start JS Build Watcher") {
-      bool success = await CroquetBuilder.EnsureJSToolsAvailable();
-      if (!success) return;
-      CroquetBuilder.StartBuild(true); // true => start watcher
-      Debug.Log("Started JS Build Watcher");
-      ToggleJSBuild_Btn.text = "Stop JS Build Watcher";
-    } else {
-      ToggleJSBuild_Btn.text = "Start JS Build Watcher";
-      CroquetBuilder.StopWatcher();
-    }
-  }
 
-  private void Clk_TakeMeToSetting() {
+    private void Clk_TakeMeToSetting() {
     // Select the file in Project pane of the Editor so it shows up in the Inspector
     var cqStgs = EnsureSettingsFile();
     if (cqStgs == null) {
@@ -378,7 +343,6 @@ public class MultisynqWelcomeEW : EditorWindow {
       Selection.activeObject = cqStgs;
     }
   }
-
 
   private void Clk_AutoSetupNode() {
     Debug.Log("Auto Setup Node!");
@@ -411,6 +375,84 @@ public class MultisynqWelcomeEW : EditorWindow {
     }
   }
 
+  private void Clk_SignUpApi() {
+    Clk_TakeMeToSetting();
+    Application.OpenURL("https://croquet.io/account/");
+  }
+
+  private async void Clk_ToggleJSBuild() {
+    if (ToggleJSBuild_Btn.text == "Start JS Build Watcher") {
+      bool success = await CroquetBuilder.EnsureJSToolsAvailable();
+      if (!success) return;
+      CroquetBuilder.StartBuild(true); // true => start watcher
+      Debug.Log("Started JS Build Watcher");
+      ToggleJSBuild_Btn.text = "Stop JS Build Watcher";
+    } else {
+      ToggleJSBuild_Btn.text = "Start JS Build Watcher";
+      CroquetBuilder.StopWatcher();
+    }
+  }
+
+  private void Clk_Top_Ready_Docs() {
+    Application.OpenURL("https://multisynq.io/docs/unity/");
+  }
+
+  private void Clk_CheckIfReady() {
+    bool allRdy = true;
+    allRdy &= Check_Settings();
+    allRdy &= Check_Node();
+    //-----
+    if (allRdy) AllAreReady();
+    else        AllAreReady(false);
+  }
+
+  private void Clk_SettingsSelect() {
+    Clk_TakeMeToSetting();
+  }
+
+  private void Clk_SettingsCreate() {
+    // CroquetSettings in scene
+    var cqStgs = EnsureSettingsFile();
+    if (cqStgs == null) {
+      Debug.LogError("Could not find or create CroquetSettings file");
+      Statuses.ready.error.Set();
+    } else {
+      Statuses.ready.success.Set();
+    }
+    Check_Settings();
+    Clk_TakeMeToSetting();
+  }
+
+  private void Clk_EnterApiKey() {
+    Clk_TakeMeToSetting();
+    var msg = @"
+In the Inspector panel,
+Enter your API Key
+into CroquetSettings.asset.
+
+Get your key here
+https://multisynq.io/
+";
+    ShowNotification(new GUIContent(msg), 4);
+  }
+
+  private async void Clk_Build_JsNow() {
+    Debug.Log("Building JS...");
+    bool success = await CroquetBuilder.EnsureJSToolsAvailable();
+    if (!success) {
+      var msg = @"
+JS Tools are missing!!! Cannot build. Please install Node.js
+using the *Try Auto Setup* button
+in the Node section.
+";
+      Debug.LogError(msg);
+      EditorUtility.DisplayDialog("Missing JS Tools", msg, "OK");
+      return;
+    }
+
+    CroquetBuilder.StartBuild(false); // false => no watcher
+  }
+
   //=============================================================================
   //=============================================================================
 
@@ -421,6 +463,7 @@ public class MultisynqWelcomeEW : EditorWindow {
       return;
     }
     button.clicked += buttonAction;
+    allButtons.Add(button);
   }
 
   private void SetupLabel(string labelName, ref Label label) {
@@ -438,7 +481,7 @@ public class MultisynqWelcomeEW : EditorWindow {
   }
 
   //=============================================================================
-  
+
   private CroquetSettings CopyDefaultSettingsFile() {
     string path = ewFolder + "resources/CroquetSettings_Template.asset";
     AssetDatabase.CopyAsset(path, cqSettingsAssetOutputPath);
@@ -479,6 +522,68 @@ public class MultisynqWelcomeEW : EditorWindow {
   
   //=============================================================================
 
+  private void AllAreReady(bool really = true) {
+    if (really) {
+      Statuses.ready.success.Set();
+      Awesome_Btn.style.visibility = Visibility.Visible;
+    } else {
+      Statuses.ready.error.Set();
+      Awesome_Btn.style.visibility = Visibility.Hidden;
+    }
+  }
+
+  //=============================================================================
+
+  private void CheckAllStatusForReady() {
+    bool allRdy = true;
+    // NEVER: allRdy &= Statuses.ready.IsOk() // NEVER want this
+    allRdy &= Statuses.settings.IsOk();
+    allRdy &= Statuses.node.IsOk();
+    // allRdy &= Statuses.key.IsOk();
+    // allRdy &= Statuses.jsBuild.IsOk()
+    if (allRdy) AllAreReady();
+    else        AllAreReady(false);
+    countdown_ToConvertSuccesses = 3f;
+  }
+
+  private bool Check_Node() {
+    var cqStgs = FindProjectCqSettings();
+    if (cqStgs == null) {
+      Statuses.node.error.Set();
+      // hide AutoSetup button
+      TryAuto_Btn.style.visibility = Visibility.Hidden;
+      return false;
+    }
+    TryAuto_Btn.style.visibility = Visibility.Visible;
+    string nodePath = cqStgs.pathToNode;
+    string nodeVer = TryNodePath(nodePath);
+    if (nodeVer == null) {
+      Statuses.node.error.Set();
+      return false;
+    } else {
+      Statuses.node.success.Set();
+      return true;
+    }
+  }
+
+  private bool Check_Settings() {
+    var cqStgs = FindProjectCqSettings();
+    if (cqStgs == null) {
+      Statuses.settings.error.Set();
+      SettingsSelect_Btn.SetEnabled(false);
+      SettingsCreate_Btn.style.visibility = Visibility.Visible;
+      return false;
+    } else {
+      TryAuto_Btn.style.visibility = Visibility.Visible;
+      SettingsSelect_Btn.SetEnabled(true);
+      Statuses.settings.success.Set();
+      SettingsCreate_Btn.style.visibility = Visibility.Hidden;
+      CheckAllStatusForReady();
+      return true;
+    }
+  }
+
+  //=============================================================================
   private void StartShScript(string scriptPath) {
     string path = EdWinPath(scriptPath);
     StartScript(path);
@@ -563,7 +668,6 @@ public class MultisynqWelcomeEW : EditorWindow {
     pcs.WaitForExit();
     if (output.Length > 0) Debug.Log(output);
     if (errors.Length > 0) Debug.LogError(errors);
-
 
     pcs.Close();
   }
