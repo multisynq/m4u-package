0846c09531793a0da320003d9fe87e4fb03f51c2
Ken <kenlane33@gmail.com>
2024-08-02 09:28:30 -0400

    FolderThing.ChildFolders(), MakeFile(), AddAppNameOutputMarker()

--- Editor/MultisynqEditorWindow/CqFile.cs
+++ Editor/MultisynqEditorWindow/CqFile.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using UnityEditor;
 using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 
 static public class CqFile {
 
@@ -77,8 +79,8 @@ static public class CqFile {
   static public FileThing CqSettingsTemplateFile() {
     return new FileThing( PkgPrefabFolder().shortPath + "/CroquetSettings_Template.asset");
   }
-  static public FolderThing StreamingAssetsAppFolder() {
-    string appNm = GetAppNameForOpenScene();
+  static public FolderThing StreamingAssetsAppFolder(string _appNm = null) {
+    string appNm = (_appNm != null) ? _appNm : GetAppNameForOpenScene();
     if (appNm == null) {
       Debug.LogError("Could not find App Name in CroquetBridge!");
       return MakeBlank();
@@ -89,6 +91,39 @@ static public class CqFile {
   }
   static public FolderThing MakeBlank() {
     return new FolderThing("");
+  }
+  static public FileThing AddAppNameOutputMarker(string appName) {
+    // add a "MyFolderIsCroquetBuildOutput.txt" file to the folder to mark it as a Croquet output folder
+    FileThing markerFile = StreamingAssetsAppFolder(appName).DeeperFile("MyFolderIsCroquetBuildOutput.txt");
+    markerFile.MakeFile("This file marks that its containing folder is a Croquet build output folder.\nDo not delete, please.\nThanks!");
+    return markerFile;
+  }
+  static public bool AppNameOutputFolderHasMarker(string appName) {
+    FileThing markerFile = StreamingAssetsAppFolder(appName).DeeperFile("MyFolderIsCroquetBuildOutput.txt");
+    return markerFile.Exists();
+  }
+  static public List<FolderThing> ListAppNameOutputFolders() {
+    var dirs = new FolderThing(Application.streamingAssetsPath).ChildFolders();
+    // filter out non-Croquet output folders without MyFolderIsCroquetBuildOutput.txt using Linq
+    return dirs.Where(dir => dir.DeeperFile("MyFolderIsCroquetBuildOutput.txt").Exists()).ToList();
+  }
+  static public void RenameToUnHideAppNameOutputFolders() {
+    foreach (FolderThing dir in ListAppNameOutputFolders()) {
+      if (dir.shortPath.Contains("~")) {
+        string newName = dir.shortPath.Replace("~", "");
+        AssetDatabase.RenameAsset(dir.shortPath, newName);
+      }
+    }
+  }
+  static public void RenameToHideAppNameOutputFoldersExceptOne(string appName) {
+    foreach (FolderThing dir in ListAppNameOutputFolders()) {
+      if (dir.shortPath.Contains(appName)) {
+        dir.SelectAndPing();
+      } else {
+        string newName = dir.shortPath + "~";
+        AssetDatabase.RenameAsset(dir.shortPath, newName);
+      }
+    }
   }
 }
 
@@ -161,6 +196,15 @@ public class FolderThing : PathyThing {
     return doesExist;
   }
 
+  public FolderThing[] ChildFolders() {
+    string[] dirs = Directory.GetDirectories(longPath);
+    FolderThing[] folders = new FolderThing[dirs.Length];
+    for (int i = 0; i < dirs.Length; i++) {
+      folders[i] = new FolderThing(dirs[i]);
+    }
+    return folders;
+  }
+
   // Deeper sub-folder
   public FolderThing DeeperFolder(params string[] deeperPaths) {    
     string newPath = longPath;
@@ -205,5 +249,9 @@ public class FileThing : PathyThing {
       Debug.LogError($"FileThing: does not exist: '{longPath}'");
     }
     return doesExist;
+  }
+  public bool MakeFile(string txt) {
+    File.WriteAllText(longPath, txt);
+    return Exists();
   }
 }
