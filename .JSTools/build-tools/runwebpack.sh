5ab4e7570ff5518dd3bc991a064ebccccba2facf
Vanessa Freudenberg <vanessa@codefrau.net>
2024-08-02 19:48:40 -0700

    Simplify JS build, and use "webview" rather than "web"

--- .JSTools/build-tools/runwebpack.sh
+++ .JSTools/build-tools/runwebpack.sh
@@ -3,11 +3,9 @@
 # the CroquetBuilder.StartBuild script supplies us with 3 or 4 arguments:
 # 1. full path to the platform-relevant node engine
 # 2. app name - used in webpack.config to find the app source
-# 3. build target ('node' or 'web' or 'webgl') - forwarded to webpack.config as buildTarget ('node'/'web') and useWebGL (true/false - only relevant for 'web')
+# 3. build target ('node' or 'webview' or 'webgl')
 # 4. full path to a temporary file to be used for watcher output (if not provided,
 #    that means we should perform a one-time build)
-
-# in preparation for packing, we set up either the webgl or non-webgl index.html
 
 DIR=`dirname "$0"`
 cd "$DIR"
@@ -15,14 +13,6 @@ cd "$DIR"
 NODE=$1
 APPNAME=$2
 TARGET=$3
-if [ "$TARGET" == "webgl" ]; then
-	TARGET="web"
-	WEBGL="true"
-	cp ./sources/index-webgl.html ./sources/index.html
-else
-	cp ./sources/index-webview_or_node.html ./sources/index.html
-	WEBGL="false"
-fi
 
 # node_modules in the CroquetJS/.js-build folder, one above here
 NODE_MODULES=../node_modules
@@ -34,7 +24,7 @@ if [ $# -eq 4 ]; then
 	# this output will be read by CroquetBuilder, to keep a record of the webpack process id
 	echo "webpack=$!"
 else
-	"$NODE" $NODE_MODULES/.bin/webpack --config webpack.config.js --mode development --env appName=$APPNAME --env buildTarget=$TARGET --env useWebGL=$WEBGL --no-color
+	"$NODE" $NODE_MODULES/.bin/webpack --config webpack.config.js --mode development --env appName=$APPNAME --env buildTarget=$TARGET --no-color
 
 	echo "webpack-exit=$?"
 fi
