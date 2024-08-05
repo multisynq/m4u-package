c3bcf22d8c029ff91dd0f8a24d072b786214769a
Ken <kenlane33@gmail.com>
2024-08-05 09:48:16 -0400

    LLM attempt at windows runweppack.bat to match runwebpack.sh

--- .JSTools/build-tools/runwebpack.bat
+++ .JSTools/build-tools/runwebpack.bat
@@ -1,13 +1,48 @@
-:: on Windows, the CroquetBuilder.StartBuild script supplies us with 3 arguments:
+:: @echo off
+:: setlocal enabledelayedexpansion
+
+:: CroquetBuilder.StartBuild script supplies us with 3 or 4 arguments:
 :: 1. full path to the node engine
 :: 2. app name - used in webpack.config to find the app source
-:: 3. build target ('node' or 'webview' or 'webgl') - also used in webpack.config
+:: 3. build target ('node' or 'webview' or 'webgl') also used in webpack.config
+:: 4. full path to a temporary file to be used for watcher output (optional)
 
-@echo off
 set nodepath=%1
 set appname=%2
 set target=%3
 
-%nodepath% ..\node_modules\webpack\bin\webpack.js --config webpack.config.js --mode development --env appName=%appname% --env buildTarget=%target% --no-color
+:: Set up TARGET and WEBGL variables
+if "%target%"=="webgl" (
+    set target=web
+    set webgl=true
+    copy .\sources\index-webgl.html .\sources\index.html
+) else (
+    set webgl=false
+    copy .\sources\index-webview_or_node.html .\sources\index.html
+)
 
-echo webpack-exit=%errorlevel%
+:: Set NODE_MODULES path
+set NODE_MODULES=..\node_modules
+
+:: Check if a logfile path is provided (4th argument)
+if "%4" neq "" (
+    set logfile=%4
+    start /B %nodepath% %NODE_MODULES%\.bin\webpack --config webpack.config.js --watch --mode development --env appName=%appname% --env buildTarget=%target% --no-color > %logfile% 2>&1
+    
+    :: Get the Process ID of the started webpack process
+    for /f "tokens=2 delims=," %%a in ('tasklist /fi "imagename eq node.exe" /fo csv /nh') do (
+        set pid=%%a
+        goto :break
+    )
+    :break
+    
+    :: Output the webpack process ID
+    echo webpack=!pid!
+) else (
+    %nodepath% %NODE_MODULES%\.bin\webpack --config webpack.config.js --mode development --env appName=%appname% --env buildTarget=%target% --env useWebGL=%webgl% --no-color
+    
+    :: Output the exit code
+    echo webpack-exit=%errorlevel%
+)
+
+:: endlocal
