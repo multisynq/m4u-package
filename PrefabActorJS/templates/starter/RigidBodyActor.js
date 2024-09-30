0ec149363650d8dff02f46d982e55229017d9211
Ken <kenlane33@gmail.com>
2024-09-30 11:58:12 -0400

    Remove templates/starter extras

--- PrefabActorJS/templates/starter/RigidBodyActor.js
+++
@@ -1,8 +0,0 @@
-//------------------------------------------------------------------------------------------
-//-- RigidBodyActor ------------------------------------------------------------------------
-//------------------------------------------------------------------------------------------
-// Some Actors also need a RigidBody. This is a mixin that adds a RigidBody to the actor.
-export class RigidBodyActor extends mix(GameActor).with(AM_RapierRigidBody) {
-  static okayToIgnore() { return [...super.okayToIgnore(), '$rigidBody'] } // reduces the warnings from snapshotting due to $ prefix
-}
-GameActor.register('RigidBodyActor');
